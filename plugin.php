9a5fa6891e249560361832606a4669a56561cbff
John Curry <johnc@tapiaproductionsinc.com>
2020-04-09 15:54:22 -0700

    vid21

---
+++ plugin.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: mytheme-blocks
+ * Plugin URI: https://alialaa.com/
+ * Description: blocks for mytheme.
+ * Author: alialaa
+ * Author URI: https://alialaa.com/
+ */
+
+?>
