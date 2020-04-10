27455c25e9b76a3e369f1c0ccdc3eddfa22d545a
John Curry <johnc@tapiaproductionsinc.com>
2020-04-09 19:07:43 -0700

    27 complete

--- dist/editor.js
+++ dist/editor.js
@@ -116,6 +116,78 @@ registerBlockType('mytheme-blocks/firstblock', {
 
 /***/ }),
 
+/***/ "./src/blocks/secondblock/index.js":
+/*!*****************************************!*\
+  !*** ./src/blocks/secondblock/index.js ***!
+  \*****************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+var _jsxFileName = "C:\\wamp64\\www\\wordpress\\wp-content\\plugins\\mytheme-blocks\\src\\blocks\\secondblock\\index.js";
+var registerBlockType = wp.blocks.registerBlockType;
+var __ = wp.i18n.__;
+registerBlockType('mytheme-blocks/secondblock', {
+  title: __('Second Block', 'mytheme-blocks'),
+  description: __('Our second block', 'mytheme-blocks'),
+  category: 'layout',
+  icon: {
+    background: '#f03',
+    foreground: '#fff',
+    src: wp.element.createElement("svg", {
+      xmlns: "http://www.w3.org/2000/svg",
+      height: "24",
+      viewBox: "0 0 24 24",
+      width: "24",
+      __self: this,
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 12,
+        columnNumber: 14
+      }
+    }, wp.element.createElement("path", {
+      d: "M0 0h24v24H0z",
+      fill: "none",
+      __self: this,
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 12,
+        columnNumber: 97
+      }
+    }), wp.element.createElement("path", {
+      d: "M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z",
+      __self: this,
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 12,
+        columnNumber: 134
+      }
+    }))
+  },
+  keywords: [__('Photo', 'mytheme-blocks'), __('Image', 'mytheme-blocks')],
+  edit: function edit() {
+    return wp.element.createElement("p", {
+      __self: this,
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 16,
+        columnNumber: 16
+      }
+    }, "Editor");
+  },
+  save: function save() {
+    return wp.element.createElement("p", {
+      __self: this,
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 19,
+        columnNumber: 16
+      }
+    }, "Saved Content");
+  }
+});
+
+/***/ }),
+
 /***/ "./src/editor.js":
 /*!***********************!*\
   !*** ./src/editor.js ***!
@@ -127,6 +199,9 @@ registerBlockType('mytheme-blocks/firstblock', {
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _blocks_firstblock__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./blocks/firstblock */ "./src/blocks/firstblock/index.js");
 /* harmony import */ var _blocks_firstblock__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_blocks_firstblock__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _blocks_secondblock__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./blocks/secondblock */ "./src/blocks/secondblock/index.js");
+/* harmony import */ var _blocks_secondblock__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_blocks_secondblock__WEBPACK_IMPORTED_MODULE_1__);
+
 
 
 /***/ })
