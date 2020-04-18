fa47eebbd2769272f8141357c748ad0e946ba55e
John Curry <fastpenguin91@gmail.com>
2020-04-18 08:18:19 -0700

    98. filter by cats

--- src/blocks/latest-posts/edit.js
+++ src/blocks/latest-posts/edit.js
@@ -2,12 +2,40 @@ import { Component } from "@wordpress/element";
 import { withSelect } from "@wordpress/data";
 import { __ } from "@wordpress/i18n";
 import { decodeEntities } from "@wordpress/html-entities";
+import { RangeControl, PanelBody, SelectControl } from "@wordpress/components";
+import { InspectorControls } from "@wordpress/editor";
 
 class LatestPostsEdit extends Component {
+    onChangeNumberOfPosts = (numberOfPosts) => {
+        this.props.setAttributes({ numberOfPosts });
+    }
+    onChangeCategories = (categories) => {
+        this.props.setAttributes({ postCategories: categories.join(',') })
+    }
     render() {
-        const { posts, className } = this.props;
+        const { posts, categories, className, attributes } = this.props;
+        const { numberOfPosts, postCategories } = attributes;
         return (
             <>
+                <InspectorControls>
+                    <PanelBody title={__("Posts Settings", "mytheme-blocks")
+                    }>
+                        <SelectControl
+                            multiple
+                            label={__('Categories', "mytheme-blocks")}
+                            onChange={this.onChangeCategories}
+                            options={categories && categories.map(category => ({ value: category.id, label: category.name }))}
+                            value={postCategories && postCategories.split(',')}
+                        />
+                        <RangeControl
+                            label={__("Number of Posts", "mytheme-blocks")}
+                            value={numberOfPosts}
+                            onChange={this.onChangeNumberOfPosts}
+                            min={1}
+                            max={5}
+                        />
+                    </PanelBody>
+                </InspectorControls>
                 {(posts && posts.length > 0) ?
                     <ul className={className}>
                         {posts.map(post => (
@@ -28,11 +56,14 @@ class LatestPostsEdit extends Component {
 export default withSelect(
     (select, props) => {
         const { attributes } = props;
-        const { numberOfPosts } = attributes;
+        const { numberOfPosts, postCategories } = attributes;
         let query = { per_page: numberOfPosts };
+        if (postCategories) {
+            query['categories'] = postCategories;
+        }
         return {
-            posts: select('core').getEntityRecords('postType', 'post',
-                { per_page: 3 })
+            posts: select('core').getEntityRecords('postType', 'post', query),
+            categories: select('core').getEntityRecords('taxonomy', 'category', { per_page: -1 })
         }
     }
 )(LatestPostsEdit);
