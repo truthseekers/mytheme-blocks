14547eb2571407252f975e9e85eddb307a0c9e62
John Curry <fastpenguin91@gmail.com>
2020-04-18 20:51:26 -0700

    116 plugin to modify gutenberg sidebar

--- src/plugins/sidebar.js
+++ src/plugins/sidebar.js
@@ -1,5 +1,9 @@
 import { registerPlugin } from "@wordpress/plugins";
-import { PluginSidebar, PluginSidebarMoreMenuItem } from "@wordpress/edit-post";
+import {
+    PluginSidebar, PluginSidebarMoreMenuItem, PluginPostStatusInfo,
+    PluginPrePublishPanel, PluginPostPublishPanel, PluginBlockSettingsMenuItem,
+    PluginMoreMenuItem
+} from "@wordpress/edit-post";
 import { __ } from "@wordpress/i18n";
 
 registerPlugin('mytheme-blocks-sidebar', {
@@ -17,6 +21,34 @@ registerPlugin('mytheme-blocks-sidebar', {
                 >
                     alkjasdf
                 </PluginSidebar>
+
+                <PluginPostStatusInfo>
+                    asldkjadsf ja ja ja
+                </PluginPostStatusInfo>
+                <PluginPrePublishPanel
+                    title="wefwef"
+                    initialOpen={true}
+                >
+                    pre publish
+                </PluginPrePublishPanel>
+                <PluginPostPublishPanel
+                    title="wefwef"
+                    initialOpen={true}
+                >
+                    Post publish
+                </PluginPostPublishPanel>
+
+                <PluginBlockSettingsMenuItem
+                    icon="twitter"
+                    label="block stuff"
+                    onClick={() => alert(true)}
+                />
+                <PluginMoreMenuItem
+                    icon="twitter"
+                    onClick={() => alert(true)}
+                >
+                    omre menu item
+                </PluginMoreMenuItem>
             </>
         );
     }
